510d963f9ce32fdb683095186c61fd51dfd2bb3e
bszhct <camel@bszhct.com>
2020-02-05 19:05:10 +0800

    将图片和字体文件打包到 js 中

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const urlConfig = {
+  limit: 9999999
+}
+
 module.exports = {
   css: {
     extract: false
@@ -14,7 +18,31 @@ module.exports = {
     //     return options
     //   })
 
+    // 禁用检测
     // config.module.rule('eslint').uses.clear()
     // config.module.rule('eslint-loader').uses.clear()
+
+    // 将字体文件打包到代码中
+    config.module
+      .rule('fonts')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, urlConfig))
+
+    // 将图片打包到代码中
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, urlConfig))
+
+    // 将 svg 图片打包到代码中
+    const svgRule = config.module.rule('svg')
+    // 清除之前存在的配置
+    svgRule.uses.clear()
+    svgRule
+      .use('url-loader')
+      .loader('url-loader')
+      .options(urlConfig)
   }
 }
